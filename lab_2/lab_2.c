498d6ecb8b42ff471dd61300a2981992ad99c42e
Ebisu02 <71516051+Ebisu02@users.noreply.github.com>
2022-11-30 22:48:11 +0700

    Update lab_2.c

--- lab_2/lab_2.c
+++ lab_2/lab_2.c
@@ -9,7 +9,7 @@ const double KB = 1024;
 const double MB = KB * 1024;
 const double GB = MB * 1024;
 
-/*
+
 void lab_2() {
     struct sysinfo systemInfo;
     sysinfo(&systemInfo);
@@ -20,4 +20,4 @@ void lab_2() {
     printf("\nTotal SWAP - %.1f GB ", systemInfo.totalswap / GB);
     printf("\nAvailable SWAP - %.1f GB \n", systemInfo.freeswap / GB);
 }
-*/
+
